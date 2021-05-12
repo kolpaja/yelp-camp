8139f1f6b57619e89ef5ca2fc631e1881d226ceb
Sokol Paja <kolpaja28@gmail.com>
2021-05-12 22:21:01 +0200

    comit to pull

--- app.js
+++ app.js
@@ -1,50 +1,52 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const Campground = require("./models/campground")
+const Campground = require("./models/campground");
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log('db: we connected!'))
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => console.log("db: we connected!"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/makecampground", async (req, res) => {
-    const camp = new Campground({ title: "bacja mbrapa shpis", description: "cheap camp" })
-    await camp.save();
-    res.send(camp);
-})
+  const camp = new Campground({
+    title: "bacja mbrapa shpis",
+    description: "cheap camp",
+  });
+  await camp.save();
+  res.send(camp);
+});
 
 app.get("/campgrounds", async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render("campgrounds/index", { campgrounds })
-})
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
 
 app.get("/campgrounds/new", (req, res) => {
-    res.render("campgrounds/new")
-})
+  res.render("campgrounds/new");
+});
 
 app.post("/campgrounds", async (req, res) => {
-    res.send(req.body)
-})
+  console.log(res.json(req.body));
+});
 
 app.get("/campgrounds/:id", async (req, res) => {
-    const campgrounds = await Campground.findById(req.params.id);
-    res.render("campgrounds/show", { campgrounds })
-})
+  const campgrounds = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campgrounds });
+});
 
 app.get("/", (req, res) => {
-    res.render("home")
-})
+  res.render("home");
+});
 
-app.listen(8080, () => console.log('Listening from port 8080'))
+app.listen(8080, () => console.log("Listening from port 8080"));
