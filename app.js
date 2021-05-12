1c0742b1567837abb24baf2bdb510bbb824fa417
kolpaja <79046681+kolpaja@users.noreply.github.com>
2021-05-12 15:32:20 +0200

    add partials/layouts &img&desc

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
+const ejsLint = require("ejs-lint");
+const ejsMate = require("ejs-mate")
 const Campground = require("./models/campground")
 const mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -12,10 +15,12 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('db: we connected!'))
 
-app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+app.use(methodOverride("_method"));
+
+app.engine("ejs", ejsMate)
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
@@ -35,7 +40,11 @@ app.get("/campgrounds/new", (req, res) => {
 })
 
 app.post("/campgrounds", async (req, res) => {
-    res.send(req.body)
+    console.log(req.body);
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+
 })
 
 app.get("/campgrounds/:id", async (req, res) => {
@@ -43,8 +52,25 @@ app.get("/campgrounds/:id", async (req, res) => {
     res.render("campgrounds/show", { campgrounds })
 })
 
+app.get("/campgrounds/:id/edit", async (req, res) => {
+    const campgrounds = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campgrounds })
+})
+
+app.put("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params
+    const campgrounds = await Campground.findByIdAndUpdate(id, req.body.campground, { runValidators: true, new: true });
+    res.redirect(`/campgrounds/${campgrounds._id}`)
+})
+
+app.delete("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params
+    const campgrounds = await Campground.findByIdAndDelete(id)
+    res.redirect("/campgrounds")
+})
+
 app.get("/", (req, res) => {
-    res.render("home")
+    res.render("campgrounds/home")
 })
 
 app.listen(8080, () => console.log('Listening from port 8080'))
