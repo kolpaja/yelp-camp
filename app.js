2807e92b382e8395f1aa25c5475c841b8e83dbd9
Sokol Paja <kolpaja28@gmail.com>
2021-05-24 00:42:27 +0200

    try to deploy

--- app.js
+++ app.js
@@ -108,4 +108,4 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Listening from port: ${port}`));
+app.listen(port, () => console.log(`Serving on port: ${port}`));
